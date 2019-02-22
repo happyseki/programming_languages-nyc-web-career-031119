ba79cbb3d424c57c864612ac78b47c76502c4105
Elaina <elainaseki@gmail.com>
2019-02-22 05:23:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,18 +1,16 @@
 def reformat_languages(languages)
- new_hash = {}
-  languages.each do |kind, data|
-new_hash ={}
+  new_hash ={}
 languages.each do |style, names|
     names.each do |name, data|
       if !new_hash.has_key?(name)
         new_hash[name]= data
         new_hash[name][:style]= []
         new_hash[name][:style].push(style)
+
        else
         new_hash[name][:style].push(style)
       end
     end
   end
   new_hash
- # your code here
 end
